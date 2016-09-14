e05cca3df46c901637b8a6abd21b6185cc7774ab
Nikhil Narula <nikhilnarula.2242@gmail.com>
2016-09-14 11:08:36 -0400

    Fix etc

--- Formula/confluent.rb
+++ Formula/confluent.rb
@@ -1,5 +1,5 @@
 class Confluent < Formula
-  desc "Confluent Platform"
+  desc "Bundles Kafka, Schema Registry, REST Proxy, Connect, etc."
   homepage "http://www.confluent.io/"
   url "http://packages.confluent.io/archive/3.0/confluent-3.0.1-2.11.zip"
   version "3.0.1"
@@ -12,9 +12,27 @@ class Confluent < Formula
     share.mkpath
     share.install Dir["share/*"]
 
-    etc.install({"etc" => "confluent"})
+    etc.install Dir["etc/*"]
   end
 
   test do
   end
+
+#  def plist; <<-EOS.undent
+#    <?xml version="1.0" encoding="UTF-8"?>
+#    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+#    <plist version="1.0">
+#    <dict>
+#    <key>Label</key>
+#      <string>#{plist_name}</string>
+#    <key>ProgramArguments</key>
+#    <array>
+#      <string>#{opt_bin}/example</string>
+#      <string>--do-this</string>
+#    </array>
+#    <key>KeepAlive</key>
+#    <true/>
+#    </plist>
+#    EOS
+#  end
 end
